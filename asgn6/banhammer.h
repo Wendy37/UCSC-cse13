fac3493f56ebb89efdebd4a596394a1fa43103e1
Wendi Tan <wtan37@ucsc.edu>
2021-03-01 12:09:12 +0800

    modify

--- asgn6/banhammer.h
+++ asgn6/banhammer.h
@@ -1,13 +1,8 @@
-//
-//  banhammer.h
-//  project
-//
-//  Created by Wendy on 2021/2/23.
-//
-
 #ifndef banhammer_h
 #define banhammer_h
 
 #include <stdio.h>
+
+int main(int argc, char **argv);
 
 #endif /* banhammer_h */
